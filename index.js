445952cb4db42a666a0fad074dea56e6be527279
joaquimbarreto <joaquim@barreto.co>
2019-01-10 16:27:51 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,37 @@ const questions = [
 ]
 
 let question;
+
+const appendQuestion = (question) => {
+  document.querySelector('.question-container').innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0];
+  appendQuestion(question);
+  return new Promise(resolve => {
+    setTimeout(() => question, time)
+  })
+}
+
+const removeQuestion = () => document.querySelector('.question-container').innerHTML = '';
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion);
+  }
+
+const trueAndFalseButtons = () => {
+  return document.querySelector('.true-false-list').querySelectorAll('.btn');
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach((item) => item.classList.toggle('hide'));
+}
+
+function displayQuestionOnClick() {
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  });
+}
